93a5b77f6a5749691864cc55127596855356fcc0
Wahabu Ramadhani Nassoro <161591965+WAHABU14@users.noreply.github.com>
2025-08-10 11:46:58 +0300

    Add files via upload

---
+++ Simple_calculator.py
@@ -0,0 +1,31 @@
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the first number: "))
+
+    operation = input("Enter the operation ")
+
+    if operation == '+':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+
+    elif operation == '-':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+
+    elif operation == '*':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+
+        else:
+            print("Error division by zero  is undefined")
+
+    else:
+        print("Invalid operation.  please choose from +,-,* and /")
+
+calculator()
+    
