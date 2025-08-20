d4fe64094abd61482747f18b6969c097be696503
Wahabu Ramadhani Nassoro <161591965+WAHABU14@users.noreply.github.com>
2025-08-20 08:52:08 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,5 +1,26 @@
--- Question 1: To create a new database called salesDB
-CREATE DATABASE salesDB;
+-- 1. Retrieve checkNumber, paymentDate, and amount from payments table
+SELECT checkNumber, paymentDate, amount
+FROM payments;
 
--- Question 2: To drop (delete) the database called demo
-DROP DATABASE demo;
+-- 2. Retrieve orderDate, requiredDate, and status of orders that are 'In Process'
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+
+-- 3. Display firstName, lastName, and email of employees who are 'Sales Rep'
+SELECT firstName, lastName, email
+FROM employees
+WHERE jobTitle = 'Sales Rep'
+ORDER BY employeeNumber DESC;
+
+-- 4. Retrieve all columns and records from offices table
+SELECT *
+FROM offices;
+
+-- 5. Fetch productName and quantityInStock from products table
+SELECT productName, quantityInStock
+FROM products
+ORDER BY buyPrice ASC
+LIMIT 5;
+
